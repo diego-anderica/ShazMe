3e7cb1c218588cdf950a5be9f336d9627d8bafed
Diego Andérica <dar-96@hotmail.com>
2017-12-10 14:35:34 +0100

    Added gradient animation in Sign In Activity and overlapping app toolbar problem solved.

--- app/src/main/java/es/uclm/esi/multimedia/shazam/SignInActivity.java
+++ app/src/main/java/es/uclm/esi/multimedia/shazam/SignInActivity.java
@@ -1,12 +1,15 @@
 package es.uclm.esi.multimedia.shazam;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -29,6 +32,9 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
         View.OnClickListener {
 
+    private AnimationDrawable animationDrawable;
+    private FrameLayout frameLayout;
+
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
     private SignInButton mSignInButton;
@@ -40,6 +46,23 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        getSupportActionBar().hide();
+
+        // init constraintLayout
+        frameLayout = (FrameLayout) findViewById(R.id.sign_in_layout);
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) frameLayout.getBackground();
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) frameLayout.getBackground();
+
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(2000);
+
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(2000);
 
         // Assign fields
         mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
@@ -135,6 +158,25 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
         // be available.
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
         Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
     }
 
 }
